8b8e68c44a837de0bfcdfbe7a881e1c7dfb4a97b
RanushMithila <ranushmithila45@gmail.com>
2023-01-12 20:57:18 +0530

    use open cv

--- ocr.py
+++ ocr.py
@@ -1,9 +1,8 @@
 import pytesseract
 import PIL.Image
-import cv2
 
 
-ocrConfig = r'--psm 3 --oem 3'
+ocrConfig = r'--psm 11 --oem 3'
 
 text = pytesseract.image_to_string(
     PIL.Image.open('img/1.jpg'), config=ocrConfig)
