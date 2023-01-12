372992b6a4b5fa51fc99c3cab0a68c1e3ccfbf09
RanushMithila <ranushmithila45@gmail.com>
2023-01-12 20:37:03 +0530

    OCR complete

--- ocr.py
+++ ocr.py
@@ -1,3 +1,11 @@
 import pytesseract
 import PIL.Image
 import cv2
+
+
+ocrConfig = r'--psm 3 --oem 3'
+
+text = pytesseract.image_to_string(
+    PIL.Image.open('img/1.jpg'), config=ocrConfig)
+
+print(text)
